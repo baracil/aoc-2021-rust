c29df2d4929b14d4fdc6222abe61a42a468b86dd
Bastien Aracil <findbastien@gmail.com>
2021-12-03 22:09:48 +0100

    solve day 3

--- src/days/day_03.rs
+++ src/days/day_03.rs
@@ -1,26 +1,141 @@
 use crate::Part;
 use crate::problem::{Problem, Result};
 
-#[allow(dead_code)]
-pub fn day03_launch(part: Part) -> Result<String> {
-    match part {
-        Part::Part1 => part1(),
-        Part::Part2 => part2()
+enum BalanceResult {
+    Value(i32),
+    Balance(i32)
+}
+
+
+struct DiagnosticReport {
+    size: usize,
+    values: Vec<i32>,
+}
+
+impl DiagnosticReport {
+    fn compute_power_consumption(&self) -> PowerConsumption {
+        let mut gamma = 0;
+
+        for i in 0..self.size {
+            let mask = 1 << i;
+
+
+            let balance = self.values.iter().fold(0, |count,value| count + get_bit_offset(value, mask));
+            if balance > 0 {
+                gamma += mask;
+            }
+        };
+        let epsilon = ((1 << self.size) - 1) ^ gamma;
+        return PowerConsumption { gamma_rate: gamma, epsilon_rate: epsilon };
+    }
+
+
+
+    fn compute_life_support(&self) -> LifeSupport {
+        let oxygen_generator_rating = self.compute_life_support_rating(|b| b>=0);
+        let co2_scrubber_rating = self.compute_life_support_rating(|b| b<0);
+
+        return LifeSupport{oxygen_generator_rating,co2_scrubber_rating}
+
+    }
+
+    fn compute_life_support_rating(&self, criteria_predicate: impl Fn(i32) -> bool) -> i32 {
+        let mut sieve_value = 0;
+        let mut sieve_mask = 0;
+        for bit_pos in (0..self.size).rev() {
+
+            let mask = 1 << bit_pos;
+            let balance_result = compute_bit_balance(&self.values, sieve_value, sieve_mask, mask);
+
+
+            match balance_result {
+                BalanceResult::Value(rating) => return rating,
+                BalanceResult::Balance(balance) =>  {
+                    let criteria_matches = criteria_predicate(balance);
+
+                    sieve_mask |= mask;
+                    if criteria_matches {
+                        sieve_value |= mask
+                    }
+                }
+            }
+        }
+        return sieve_value
     }
 }
 
-fn part1() -> Result<String> {
-    Err("Not Solved Yet".to_string())
+
+fn get_bit_offset(value:&i32, bit_mask:i32) -> i32 {
+    if (value & bit_mask) == 0 { -1 } else {  1 }
 }
 
-fn part2() -> Result<String> {
-    Err("Not Solved Yet".to_string())
+fn compute_bit_balance(values: &Vec<i32>, sieve_value: i32, sieve_mask: i32, bit_mask: i32) -> BalanceResult {
+    let mut balance = 0;
+    let mut nb_matches = 0;
+    let mut a_value = -1;
+    for value in values {
+
+        let value_match = (*value & sieve_mask) == sieve_value;
+
+        if !value_match {
+            continue
+        }
+
+        nb_matches += 1;
+        a_value = *value;
+        balance += get_bit_offset(value,bit_mask);
+    };
+
+    match nb_matches {
+        1 => BalanceResult::Value(a_value),
+        _ => BalanceResult::Balance(balance)
+    }
+
 }
+
+struct PowerConsumption {
+    gamma_rate: i32,
+    epsilon_rate: i32,
+}
+
+struct LifeSupport {
+    oxygen_generator_rating: i32,
+    co2_scrubber_rating: i32,
+}
+
 
 #[allow(dead_code)]
-fn parse_input(for_test:bool) -> Result<String> {
-    Problem::factory(for_test)(3).read_input()
+pub fn day03_launch(part: Part) -> Result<String> {
+    let diagnostics = parse_input(false)?;
+    match part {
+        Part::Part1 => Ok(part1(&diagnostics).to_string()),
+        Part::Part2 => Ok(part2(&diagnostics).to_string())
+    }
 }
+
+fn part1(diagnostics: &DiagnosticReport) -> i32 {
+    let rate = diagnostics.compute_power_consumption();
+    rate.gamma_rate * rate.epsilon_rate
+}
+
+fn part2(diagnostics: &DiagnosticReport) -> i32 {
+    let rate = diagnostics.compute_life_support();
+    rate.oxygen_generator_rating*rate.co2_scrubber_rating
+}
+
+
+fn parse_input(for_test: bool) -> Result<DiagnosticReport> {
+    let lines = Problem::factory(for_test)(3).read_input_as_vec_of_line()?;
+    let size = lines.get(0).map_or(0, |l| l.len());
+
+    if size == 0 {
+        return Err("No data".to_string());
+    }
+
+    let values = lines.iter().map(|l| i32::from_str_radix(l, 2).unwrap()).collect();
+    Ok(DiagnosticReport { size, values })
+}
+
 
 #[cfg(test)]
 #[allow(dead_code)]
@@ -28,16 +143,16 @@ mod tests {
     use crate::days::day_03::{parse_input, part1, part2};
 
     #[test]
-    fn day03_part1_test()  {
-        let _input = parse_input(true).unwrap();
-        let result = part1().unwrap();
-        assert_eq!(result,"")
+    fn day03_part1_test() {
+        let diagnostic = parse_input(true).unwrap();
+        let result = part1(&diagnostic);
+        assert_eq!(result, 198)
     }
 
     #[test]
-    fn day03_part2_test()  {
-        let _input = parse_input(true).unwrap();
-        let result = part2().unwrap();
-        assert_eq!(result,"")
+    fn day03_part2_test() {
+        let diagnostic = parse_input(true).unwrap();
+        let result = part2(&diagnostic);
+        assert_eq!(result, 230)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crate::days::day_02::day02_launch;
+use crate::days::day_03::day03_launch;
 use crate::problem::Part;
 
 mod problem;
@@ -8,7 +9,7 @@ mod days;
 
 #[warn(dead_code)]
 fn main() {
-    let launch = day02_launch;
+    let launch = day03_launch;
 
     println!("part 1 : {:?}",launch(Part::Part1));
     println!("part 2 : {:?}",launch(Part::Part2));
