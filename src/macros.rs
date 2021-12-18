03e3010634b0f62d365fa79ed53c701c839fcd6a
Bastien Aracil <findbastien@gmail.com>
2021-12-18 14:37:54 +0100

    add parsing macro

--- src/macros.rs
+++ src/macros.rs
@@ -1,2 +1,5 @@
 #[macro_export]
 macro_rules! parse_input { ($x:expr, $t:ident) => ($x.trim().parse::<$t>().expect(&format!("Cannot parse '{}'",$x))) }
+
+#[macro_export]
+macro_rules! try_parse_input { ($x:expr, $t:ident) => ($x.trim().parse::<$t>().map_err(|o| format!("Cannot parse {} : {:?}",$x,o))) }
